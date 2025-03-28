31fbe8894aa6c9d7871018a483da7ea395063d64
Ayguid-meta <guido.aimar@metajive.com>
2025-03-28 19:42:24 -0300

    init-setup

---
+++ resources/js/pages/Phonebook.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Phonebook">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <div class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] dark:bg-[#0a0a0a] lg:justify-center lg:p-8">
+        <header class="not-has-[nav]:hidden mb-6 w-full max-w-[335px] text-sm lg:max-w-4xl">
+            <nav class="flex items-center justify-end gap-4">
+
+            </nav>
+        </header>
+        <div class="duration-750 starting:opacity-0 flex w-full items-center justify-center opacity-100 transition-opacity lg:grow">
+            <main class="flex w-full max-w-[335px] flex-col-reverse overflow-hidden rounded-lg lg:max-w-4xl lg:flex-row">
+                <div
+                    class="flex-1 rounded-bl-lg rounded-br-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] lg:rounded-br-none lg:rounded-tl-lg lg:p-20"
+                >
+                    <h1 class="mb-1 font-medium">Let's get started</h1>
+                    <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">
+                        Laravel has an incredibly rich ecosystem. <br />We suggest starting with the following.
+                    </p>
+
+                </div>
+                <div
+                    class="relative -mb-px aspect-[335/376] w-full shrink-0 overflow-hidden rounded-t-lg bg-[#fff2f2] dark:bg-[#1D0002] lg:-ml-px lg:mb-0 lg:aspect-auto lg:w-[438px] lg:rounded-r-lg lg:rounded-t-none"
+                >
+
+                    <div
+                        class="absolute inset-0 rounded-t-lg shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] lg:overflow-hidden lg:rounded-r-lg lg:rounded-t-none"
+                    />
+                </div>
+            </main>
+        </div>
+        <div class="h-14.5 hidden lg:block"></div>
+    </div>
+</template>
