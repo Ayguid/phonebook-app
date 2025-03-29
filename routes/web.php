d8bc6cbbe6de9bf57c76b590160b5d179c21eec9
Ayguid <guidoaimar@gmail.com>
2025-03-29 04:13:54 -0300

    feat: added rate limit for routes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,11 @@ use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', [ContactController::class, 'index'])->name('home');
-Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
-Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
 
-require __DIR__.'/settings.php';
+Route::middleware('throttle:60,1')->group(function () {
+    // These routes will have a rate limit of 60 requests per minute
+    Route::get('/', [ContactController::class, 'index'])->name('home');
+    Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+    Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+});
+
